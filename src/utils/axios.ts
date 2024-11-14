336f65a12b48d10df118872d9ba5d12fdf60f99f
shpokas <s.shpokas@gmail.com>
2024-11-14 12:26:35 +0200

    fix(axios): configure axios instance with base URL and token interceptor, resolve type issues

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-instance.interceptors.request.use((config) => {
+instance.interceptors.request.use((config: AxiosRequestConfig) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers = { ...config.headers, Authorization: `Bearer ${token}` };
   }
   return config;
 });
