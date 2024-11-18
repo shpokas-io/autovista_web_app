a11f4f1da16c7e7e5076131aeac2a705647e112d
shpokas <s.shpokas@gmail.com>
2024-11-18 17:48:11 +0200

    feat: assemble basic HomePage with NavigationBar, sections, and Footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,20 @@
-function HomePage() {
-  return <h1>Welcome to Home Page</h1>;
-}
+import React from "react";
+import NavigationBar from "../components/layout/NavigationBar";
+import Footer from "../components/layout/Footer";
+import HeroSection from "../components/HomePageSections/HeroSection";
+import FeaturesSection from "../components/HomePageSections/FeaturesSection";
+import HowItWorksSection from "../components/HomePageSections/HowItWorks";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <NavigationBar />
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;
