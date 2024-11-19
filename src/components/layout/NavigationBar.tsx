62f02dce29ede9b2bf80cbf3c9fa68568d98116b
shpokas <s.shpokas@gmail.com>
2024-11-19 14:31:56 +0200

    feat(navbar): Updated navbar to be transparrent and added burger menu

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -1,22 +1,77 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const NavigationBar: React.FC = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open);
+  };
+
+  const drawer = (
+    <Box onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+      <List>
+        <ListItem component="div">
+          <ListItemButton component={NavLink} to="/">
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem component="div">
+          <ListItemButton component={NavLink} to="/cars">
+            <ListItemText primary="Cars" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
-    <AppBar position="sticky" color="primary">
+    <AppBar position="sticky" color="transparent" elevation={0}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Digital Car Garage
         </Typography>
-        <Box>
-          <Button color="inherit" component={NavLink} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={NavLink} to="/cars">
-            Cars
-          </Button>
-        </Box>
+        {isDesktop ? (
+          <Box sx={{ display: "flex", justifyContent: "center", flexGrow: 1 }}>
+            <Button color="inherit" component={NavLink} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={NavLink} to="/cars">
+              Cars
+            </Button>
+          </Box>
+        ) : (
+          <>
+            <IconButton color="inherit" edge="end" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={drawerOpen}
+              onClose={toggleDrawer(false)}
+            >
+              {drawer}
+            </Drawer>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
