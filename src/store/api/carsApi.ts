b8b6193f7b273bcf44b8ad00941faf74676597a6
shpokas <s.shpokas@gmail.com>
2024-11-25 13:14:46 +0200

    fix: remove `any` from carsApi and specify Car type for API response

--- src/store/api/carsApi.ts
+++ src/store/api/carsApi.ts
@@ -1,8 +1,18 @@
 import baseApi from "./baseApi";
 
+interface Car {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  car_image: string;
+  transmission: string;
+  power: number;
+}
+
 export const carsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getCars: builder.query<any, void>({
+    getCars: builder.query<Car[], void>({
       query: () => "/cars",
     }),
   }),
